4e998f270b68c9d73ba7496268a575c850b45015
prism97 <priyetasaha@gmail.com>
2021-04-23 11:35:48 +0600

    Recreate mcq ui using base exercise screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,12 @@
 import 'package:vasha_shikkha/ui/fb/fill_in_the_blanks_view.dart';
 import 'package:vasha_shikkha/ui/home/home_screen.dart';
 import 'package:vasha_shikkha/ui/login/login_screen.dart';
-import 'package:vasha_shikkha/ui/mcq/mcq_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:vasha_shikkha/ui/mcq/multiple_choice_view.dart';
 
 final routes = {
   '/login': (BuildContext context) => LoginScreen(),
   HomeScreen.id: (BuildContext context) => HomeScreen(),
-  '/mcq': (BuildContext context) => McqScreen(),
+  '/mcq': (BuildContext context) => MultipleChoiceView(),
   '/fb': (BuildContext context) => FillInTheBlanksView(),
 };
---
+++ lib/ui/mcq/multiple_choice_view.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:vasha_shikkha/ui/base/correct_dialog.dart';
+import 'package:vasha_shikkha/ui/base/exercise_screen.dart';
+
+class MultipleChoiceView extends StatefulWidget {
+  @override
+  _MultipleChoiceViewState createState() => _MultipleChoiceViewState();
+}
+
+class _MultipleChoiceViewState extends State<MultipleChoiceView> {
+  final String question =
+      "Rehnuma has just received the result of her final examination of class 7. She has stood first in her class. She is now-";
+
+  final List<String> _options = ["sad", "excited", "happy", "on cloud nine"];
+
+  final String _correctOption = "on cloud nine";
+  int _selectedOption = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExerciseScreen(
+      exerciseName: "Multiple Choice Question",
+      exercise: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(question),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 20,
+              ),
+              _buildOptions(),
+            ],
+          ),
+        ),
+      ),
+      onCheck: _onCheck,
+    );
+  }
+
+  ListView _buildOptions() {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: _options.length,
+      separatorBuilder: (context, index) => SizedBox(
+        height: 10,
+      ),
+      clipBehavior: Clip.antiAlias,
+      itemBuilder: (context, index) {
+        return Card(
+          elevation: index == _selectedOption ? 20 : 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          color: index == _selectedOption
+              ? Theme.of(context).accentColor
+              : Colors.white,
+          child: ListTile(
+            selected: index == _selectedOption,
+            title: Text(
+              _options[index],
+              style: TextStyle(color: Colors.black),
+            ),
+            onTap: () {
+              if (index == _selectedOption) {
+                setState(() {
+                  _selectedOption = -1;
+                });
+              } else {
+                setState(() {
+                  _selectedOption = index;
+                });
+              }
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  bool _onCheck() {
+    if (_selectedOption == -1) return false;
+    return _options[_selectedOption].compareTo(_correctOption) == 0;
+  }
+}
