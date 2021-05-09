7d73737f99d4db48506f3a101850ccc8b0304229
prism97 <priyetasaha@gmail.com>
2021-05-09 18:59:12 +0600

    Delete moor database

--- lib/data/tables/token.dart
+++
@@ -1,5 +0,0 @@
-import 'package:moor_flutter/moor_flutter.dart';
-
-class Tokens extends Table {
-  TextColumn get token => text()();
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'package:vasha_shikkha/data/moor_database.dart';
 import 'package:vasha_shikkha/routes.dart';
 import 'package:vasha_shikkha/style/custom_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:vasha_shikkha/ui/home/home_screen.dart';
-import 'package:vasha_shikkha/ui/login/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,19 +10,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<TokensDao>(
-          create: (context) => MoorDatabase().tokensDao,
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Vasha Shikkha',
-        theme: customThemeData,
-        routes: routes,
-        initialRoute: '/login',
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vasha Shikkha',
+      theme: customThemeData,
+      routes: routes,
+      initialRoute: '/login',
     );
   }
 }
